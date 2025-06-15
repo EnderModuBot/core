332237e1f8623922cade912cd5768f93e877f95f
Endkind <endkind.ender@gmail.com>
2025-06-15 22:27:52 +0200

    Füge Dokumentation zu Klassen und Methoden hinzu und erweitere die Release-Drafter-Konfiguration um Dokumentationskategorie

--- ModuBotCore/__init__.py
+++ ModuBotCore/__init__.py
@@ -11,12 +11,29 @@ from .modules import BaseModule
 
 
 class ModuBotCore(BaseConfig):
+    """
+    Core class for the ModuBot framework.
+
+    Handles logging, module loading, and lifecycle management (run/stop).
+    Modules are expected to reside in the "modules" directory and inherit from the defined MODULE_BASE_CLASS.
+
+    :cvar NAME: The name of the bot core.
+    :cvar VERSION: Current version of the bot core.
+    :cvar LOGGER_CONFIG: Logger configuration class.
+    :cvar MODULE_BASE_CLASS: Base class for all modules to be loaded.
+    """
+
     NAME: ClassVar[str] = "ModuBotCore"
     VERSION: ClassVar[str] = "0.0.1"
     LOGGER_CONFIG: ClassVar[Type[LoggerConfig]] = LoggerConfig
     MODULE_BASE_CLASS: ClassVar[Type[BaseModule]] = BaseModule
 
     def __init__(self):
+        """
+        Initializes logging and prepares the module list.
+
+        Registers the stop() method to be called automatically on application exit.
+        """
         logging.basicConfig(
             level=self.LOGGER_CONFIG.LEVEL,
             format=self.LOGGER_CONFIG.FORMAT,
@@ -26,6 +43,11 @@ class ModuBotCore(BaseConfig):
         atexit.register(self.stop)
 
     def run(self):
+        """
+        Starts the bot and enables all loaded modules.
+
+        Calls the on_enable() method of each module after loading.
+        """
         self.logger.info(f"Starting {self.NAME}")
         self._load_modules()
         for module in self.modules:
@@ -33,12 +55,24 @@ class ModuBotCore(BaseConfig):
             module.on_enable()
 
     def stop(self):
+        """
+        Disables all loaded modules and logs shutdown.
+
+        Calls the on_disable() method of each module.
+        """
         for module in self.modules:
             self.logger.info(f'Disabling module "{module.NAME}"')
             module.on_disable()
         self.logger.info(f"Stopping {self.NAME}")
 
     def _load_modules(self):
+        """
+        Dynamically loads all modules from the "modules" directory.
+
+        Each module must be in its own directory with an `__init__.py` file.
+        The module must contain a class that inherits from MODULE_BASE_CLASS.
+        Only modules with ENABLING = True will be instantiated and added.
+        """
         root = Path().resolve()
         module_dir = root / "modules"
         self.logger.debug(f'Loading modules from "{module_dir}"')
@@ -78,4 +112,10 @@ class ModuBotCore(BaseConfig):
 
     @property
     def logger(self) -> logging.Logger:
+        """
+        Returns the logger instance for the bot.
+
+        :return: Logger bound to the bot's NAME.
+        :rtype: logging.Logger
+        """
         return logging.getLogger(self.NAME)
